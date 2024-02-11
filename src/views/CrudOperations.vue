1c042f0dae5604c4370b4349f24e36ae56133d24
Mohamad AbdelMon’em <mabdelmonem.c@saip.gov.sa>
2024-02-11 11:05:14 +0200

    ue tailwind and fetch posts data

--- src/views/CrudOperations.vue
+++ src/views/CrudOperations.vue
@@ -1,3 +1,18 @@
 <template>
   <h2>This is a crud operations</h2>
+  <div>
+    <div class="my-4" v-for="post in posts">
+      <h2 class="text-2xl mb-2">{{ post.title }}</h2>
+      <p>{{ post.body }}</p>
+    </div>
+  </div>
 </template>
+
+<script setup>
+  import { ref } from 'vue'
+
+  let posts = ref(null)
+  fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((res) => res.json())
+    .then((data) => (posts.value = data))
+</script>
